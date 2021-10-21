ff97560ed247f443c425b79a8c0bb7eb4d295401
Saurabh Agarwal <saurabhagarwal.official@gmail.com>
2021-10-21 17:55:40 +0530

    Basic Selenium driver setting

---
+++ Basic/basic.py
@@ -0,0 +1,5 @@
+from selenium import webdriver          #calling webdriver functionality
+
+chrome_driver = "/Users/saurabhagarwal/PycharmProjects/Python_Selenium/Selenium_Driver/chromedriver"
+chrome_drive = "..//Selenium_Driver//chromedriver"
+driver = webdriver.Chrome(chrome_driver)          #initializing which browser to use for pop up when automation runs
